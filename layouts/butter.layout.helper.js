62aeddb56638f393087631db290684583e5c3226
secretrobotron <secretrobotron@gmail.com>
2011-06-16 11:49:23 -0400

    testing environment for layouts and media list for layout helper

--- layouts/butter.layout.helper.js
+++ layouts/butter.layout.helper.js
@@ -1,10 +1,17 @@
 (function (window, document) {
 
+  var LayoutHelpers = window.LayoutHelpers = [];
+
   var LayoutHelper = window.LayoutHelper = function () {
 
     var that = this,
-        targets = this.targets = {},
-        markedObjects = document.getElementsByClassName( "butter-layout-target" );
+        markedObjects = document.getElementsByClassName( "butter-layout-target" ),
+        markedMedia = document.getElementsByClassName( "butter-layout-media" );
+
+    LayoutHelpers.push( this );
+
+    this.targets = {};
+    this.media = [];
     
     this.addTarget = function ( target ) {
 
@@ -13,9 +20,19 @@
       } //if
     }; //addTarget
 
+    this.addMedia = function ( media ) {
+
+      that.media.push(media);
+    }; //addMedia
+
     for ( var i=0; i<markedObjects.length;++i ) {
       
       this.addTarget( markedObjects[ i ] );
+    } //for
+
+    for ( var i=0; i<markedMedia.length;++i ) {
+      
+      this.addMedia( markedMedia[ i ] );
     } //for
 
     this.send = function ( message, data ) {
@@ -29,14 +46,21 @@
 
 
     var functions = {
+
       "get targets": function ( data ) {
-        that.send( "receive targets", targets.keys );
+
+        var keys = [];
+        for ( var k in targets ) {
+          keys.push(k);
+        } //for
+        that.send( "receive targets", keys );
       },
+
     };
 
     window.addEventListener( "message", function (e) {
 
-      if ( e.data && e.data[ "layout-helper"] === true ) {
+      if ( e.data && e.data[ "layout-helper-connection"] === true ) {
 
         if ( e.data.message in functions ) {
           functions[ e.data.message ]( e.data.data );
@@ -54,21 +78,40 @@
     this.send = function ( message, data ) {
 
       commObject.contentWindow.postMessage({
-        "layout-helper": true,
+        "layout-helper-connection": true,
         message: message,
         data: data,
       }, "*");
     }; //send
 
+    var functions = {
+
+      "receive targets": function ( data ) {
+
+      },
+
+      "media timeupdate": function ( data ) {
+      },
+    };
+
     commObject.contentWindow.addEventListener( "message", function (e) {
-      console.log(e.data);
 
       if ( e.data && e.data[ "layout-helper"] === true ) {
+        
+        if ( e.data.message in functions ) {
+
+          functions[ e.data.message ]( e.data.data );
+
+        } //if
+
       } //if
 
     }, false);
 
-    this.send( "get targets" );
+    this.getTargets = function () {
+
+      this.send( "get targets" );
+    }; //getTargets
 
   };
 
